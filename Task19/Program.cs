4c95fea15eba3e416354a40bb4599101e7f49877
Yaroslav63 <era6300@mail.ru>
2022-03-10 16:07:27 +0300

    add Task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = 0;
+while (true)
+{
+    Console.Write("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    else
+        Console.WriteLine("Неверное число.");
+}
+int oldValue = number;
+int newValue = 0;
+while (number > 0)
+{
+    int dig = number % 10;
+    newValue = newValue * 10 + dig;
+    number = number / 10;
+}
+if (newValue == oldValue)
+    Console.WriteLine("Число является палиндромом");
+else
+    Console.WriteLine("Число не является палиндромом");
