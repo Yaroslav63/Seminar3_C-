ea592d04ba9e9e6982c0e325cb66961027e41c4e
Yaroslav63 <era6300@mail.ru>
2022-03-10 15:49:35 +0300

    add Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    int ind = i * i * i;
+    Console.WriteLine(ind + " ");
+}
